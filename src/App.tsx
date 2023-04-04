041fe2ca665a6eac430819623ff6e2da3170e73d
Danh Nguyen <danh.nguyentrancong>
2023-04-04 23:11:59 +0700

    Initial Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { GradientText } from './components/Gradient';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<GradientText text="Initial Project React With Typescript and TailwindCss" />
+		</div>
+	);
 }
 
 export default App;
